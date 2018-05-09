5ecbde4f0e0d68d6ada2209728bd0190a4294c5d
amit05007 <amit05007@gmail.com>
2018-05-09 22:34:14 +0530

    click event added from cloned repo

--- CalculatorGit/Form1.cs
+++ CalculatorGit/Form1.cs
@@ -16,5 +16,10 @@ namespace CalculatorGit
         {
             InitializeComponent();
         }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
